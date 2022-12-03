39d40fd2dda00736a408d43f8b661499f3997752
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-12-03 15:44:18 +0300

    дз8

---
+++ src/main/java/com/skypro/integerlist/Main.java
@@ -0,0 +1,7 @@
+package com.skypro.integerlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/skypro/integerlist/service/IntegerService.java
@@ -0,0 +1,73 @@
+package com.skypro.integerlist.service;
+
+public class IntegerService implements IntegerList{
+    @Override
+    public Integer add(Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer set(int index, Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Integer item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return new Integer[0];
+    }
+}
