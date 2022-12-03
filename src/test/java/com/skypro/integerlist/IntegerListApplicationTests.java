2cea6196bcd4cf08027349e20f825aa218a9d834
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-12-03 15:37:21 +0300

    дз8

---
+++ src/test/java/com/skypro/integerlist/IntegerListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.integerlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
